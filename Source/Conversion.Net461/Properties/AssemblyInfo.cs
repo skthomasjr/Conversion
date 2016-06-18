d9a7f05d1704654864d2f513e0e322abeee80fef
Scott Keith Thomas, Jr <sthomas@idis.com>
2016-06-17 22:56:34 -0600

    Initial commit.

---
+++ Source/Conversion.Net461/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Conversion.NET")]
+[assembly: AssemblyDescription("Conversion.NET is a type conversion library.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Scott Keith Thomas, Jr.")]
+[assembly: AssemblyProduct("Conversion.NET")]
+[assembly: AssemblyCopyright("Copyright © 2016 Scott Keith Thomas, Jr.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d0353061-2b12-4c8e-ba12-c2b200fab5ed")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
